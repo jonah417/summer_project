2c5004aac7ada2a7491e501323f88a5ca8e809c2
jonah417 <jrm36@st-andrews.ac.uk>
2022-06-06 13:09:06 +0000

    add output options and improve cluster check

--- mcds_input/create_command_file.r
+++ mcds_input/create_command_file.r
@@ -1,5 +1,9 @@
-# creating a set of test inputs, in the same form as those used in ddf.R
+# assuming the inputs are in the same form as those used in ddf.R
 
+# data must have the following fields:
+# object, observer, detected, distance (matches mrds input)
+# SMP_LABEL, SMP_EFFORT (required for mcds)
+# optionally
 
 create_command_file(dsmodel=call(),mrmodel=call(),data,method,
                     meta.data,control) {
@@ -21,6 +25,8 @@ create_command_file(dsmodel=call(),mrmodel=call(),data,method,
   # fill in option section
   # !find whether cue counting was used, in which case TYPE=CUE
   
+  # !this needs redone with meta.data
+  # !consider the case where TYPE="LINE" but DISTANCE="RADIAL"?
   transect_type <- toupper(data$transect)
   cat("TYPE=", transect_type, ";",
       file=command.file.name, "\n", append=TRUE)
@@ -36,15 +42,32 @@ create_command_file(dsmodel=call(),mrmodel=call(),data,method,
   # !if cue counts are used, state cue rate and SE
   
   # define whether there are clusters
-  if(grepl("(^|,)size($|,)", data$fieldnames)){
+  if(TRUE %in% grepl("^size$",colnames(data))){
+    # the fact that data is clustered is used later on, so this
+    # boolean prevents us from having to check each time
+    cluster <- TRUE
     cat("OBJECT=CLUSTER;", file=command.file.name, "\n", append=TRUE)
   }else{
     cat("OBJECT=SINGLE;", file=command.file.name, "\n", append=TRUE)
   }
   
+  # managing the output options
+  if(control$debug == TRUE){
+    cat("DEBUG=TRUE;", file=command.file.name, "\n", append=TRUE)
+  }
+  
+  # !not sure if the output levels match completely
+  output_info_levels <- c("SUMMARY","RESULTS","SELECTION","ALL")
+  specified_output_level <- output_info_levels[data$showit-1]
+  cat(specified_output_level, file=command.file.name, "\n", append=TRUE)
+  
+  # !the selection section for model fitting; is that specified in ddf?
+  
   # moving onto the data section
   
   cat("END;", file=command.file.name, "\n", append=TRUE)
   cat("DATA /STRUCTURE=FLAT;", file=command.file.name, "\n", append=TRUE)
+  
+  
   
 }
