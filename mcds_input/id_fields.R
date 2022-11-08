7e04f9359b2bc6acce1f0a0c1c534f0871022584
jonah417 <jrm36@st-andrews.ac.uk>
2022-11-08 18:06:40 +0000

    write function for identifying columns within the dataframe

--- mcds_input/id_fields.R
+++ mcds_input/id_fields.R
@@ -1,2 +1,31 @@
 # Identify Field Names Function
 
+# parameters:
+# data_cols - vector of original column names
+# fin_name - string of desired final name of the column
+# pot_names - vector of potential names the column may originally have
+
+id_fields <- function(data_cols, fin_name, pot_names) {
+  # first, identify which column contains the relevant data
+  # we want to check through each of the potential names to see which appears
+  # in the column names, so we need to know the number of potential names and
+  # keep track of whether it is present
+  name_pres <- FALSE
+  # convert vector of potential names to lower case for ease of comparison
+  pot_names <- tolower(pot_names)
+  # check through each of the potential names to identify if it is used for a
+  # column
+  for(i in 1:length(pot_names)) {
+    if(TRUE %in% grepl(pot_names[i],tolower(data_cols))) {
+      # identify the index of the column that needs renamed
+      name_pres <- grep(pot_names[i],tolower(data_cols))
+    }
+  }
+  # return either that the name wasn't found, or the index it was found at
+  return(name_pres)
+}
+
+
+
+
+
--- mcds_input/reformat_data.R
+++ mcds_input/reformat_data.R
@@ -4,8 +4,39 @@
 # in the command file is in the correct format
 
 reformat_data <- function(data, dsmodel){
+  # firstly, identifying if the data has multiple observers
+  if(TRUE %in% grepl("^detected$",tolower(colnames(data)))){
+    # combine data from multiple observers
+    data <- data[data$detected==1,]
+    if(TRUE %in% grepl("^object$",tolower(colnames(data)))){
+      # in case the object column isn't named 'object', to remove case sensitivity
+      obj_col <- grep("^object$",tolower(colnames(data)))
+      colnames(data)[obj_col] <- "object"
+      # identifying all objects and taking the first data point for each
+      obj_nums <- unique(data$object)
+      for(i in 1:length(obj_nums)){
+        entries <- grep(TRUE,data$object==i)
+        if(length(entries)>1){
+          remove <- entries[-1]
+          data <- data[-remove,]
+        }
+      }
+    }
+  }
+  
+  # matching the names to those used in MCDS
+  # create a list of required fields and likely alternative names
+  req_fields <- list(list("SMP_LABEL", c("SMP_LABEL","sample.label")),"SMP_EFFORT","DISTANCE")
+  # create a vector of optional specified fields
+  opt_fields <- c("STR_LABEL","STR_AREA","size")
+  
+  # change the distance column name to upper case
+  colnames(data)[grep("^distance$",tolower(colnames(data)))] <- "DISTANCE"
+  
   # function for field names
-  # function for covars
+  # rename columns and deal with required variables
+  # check if covars present
+    # function for covars
   # creating the dataframe
   # output new dataframe
 }
