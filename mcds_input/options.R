5491a287e7530d459da8a9356e51ae89540e2402
jonah417 <jrm36@st-andrews.ac.uk>
2023-01-11 14:36:20 +0000

    Save options.R function to the correct folder

---
+++ mcds_input/options.R
@@ -0,0 +1,38 @@
+# Options Section
+
+# A function which writes the OPTIONS section to the command file, based upon
+# the mrds input
+
+# Inputs:
+#  - meta.data: a list as found in mrds
+#  - control: a list as found in mrds
+
+options <- function(meta.data, control) {
+  # starting the options section within the command file
+  cat("OPTIONS;", file=command.file.name, "\n", append=TRUE)
+  
+  # creating a list with all the options and results
+  opt_list <- list(point1=list(var=meta.data$point,
+                               results=list("TYPE=POINT;","TYPE=LINE;")),
+                   point2=list(var=meta.data$point,
+                              results=list(c("DISTANCE=RADIAL /UNITS='Meters' /WIDTH=", 
+                                             meta.data$width, ";"), 
+                                           c("DISTANCE=PERP /UNITS='Meters' /WIDTH=", 
+                                             meta.data$width, ";"))),
+                   cluster=list(var=cluster,
+                                results=list("OBJECT=CLUSTER;","OBJECT=SINGLE;")),
+                   debug=list(var=control$debug,
+                              results=list("DEBUG=ON;","")))
+  
+  # looping through each of the options and concatenating the relevant commands
+  for(i in 1:length(opt_list)) {
+    # inputting the relevant data to the cat_conditions function
+    cat_conditions(opt_list[[i]][[1]],opt_list[[i]][[2]])
+  }
+  
+  # the user will specify the adjustment term selection
+  cat("SELECTION=SPECIFY;", file=command.file.name, "\n", 
+      append=TRUE)
+  # ending the options section
+  cat("END;", file=command.file.name, "\n", append=TRUE)
+}
